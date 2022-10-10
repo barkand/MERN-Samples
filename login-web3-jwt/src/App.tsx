f222138ed4f256d391e62957919dbe6e7f1233ec
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-10-10 22:48:54 +0330

    Add SWR & Web3

--- login-web3-jwt/src/App.tsx
+++ login-web3-jwt/src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import React from "react";
+
 import "./css/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = React.useState(0);
 
   return (
     <div className="App">

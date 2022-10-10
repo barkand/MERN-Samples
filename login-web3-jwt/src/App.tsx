8ba07de6314fe398dc4c0506d432823d0be2910b
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-10-10 22:27:45 +0330

    init login-web3-jwt

---
+++ login-web3-jwt/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./css/App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

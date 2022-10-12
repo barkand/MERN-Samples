712d010a6daeb538450e4a86c95273e80c35eb99
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-10-13 01:09:57 +0330

    Fix JWT .env

--- login-web3-jwt/server/global.d.ts
+++ login-web3-jwt/server/global.d.ts
@@ -5,5 +5,9 @@ declare namespace NodeJS {
     DB_NAME: string;
     DB_USER: string;
     DB_PASSWORD: string;
+    SECRET_KEY: string;
+    SECRET_KEY_LIFE_TIME: string;
+    REFRESH_SECRET_KEY: string;
+    REFRESH_SECRET_KEY_LIFE_TIME: string;
   }
 }
--- login-web3-jwt/server/src/controllers/jwt.ts
+++ login-web3-jwt/server/src/controllers/jwt.ts
@@ -1,18 +1,13 @@
 import jwt, { Secret } from "jsonwebtoken";
 
-const secret_key: Secret = `${process.env.SECRET_KEY}`;
-const refresh_secret_key: Secret = `${process.env.REFRESH_SECRET_KEY}`;
-const secret_key_life_time: any = `${process.env.SECRET_KEY_LIFE_TIME}`;
-const refresh_secret_key_life_time: any = `${process.env.REFRESH_SECRET_KEY_LIFE_TIME}`;
-
 const createToken = async (username: string) => {
   let payload: any = { username: username };
 
-  const token = jwt.sign(payload, secret_key, {
-    expiresIn: "1h",
+  const token = jwt.sign(payload, `${process.env.SECRET_KEY}`, {
+    expiresIn: `${process.env.SECRET_KEY_LIFE_TIME}`,
   });
-  const refreshToken = jwt.sign(payload, refresh_secret_key, {
-    expiresIn: "1d",
+  const refreshToken = jwt.sign(payload, `${process.env.REFRESH_SECRET_KEY}`, {
+    expiresIn: `${process.env.REFRESH_SECRET_KEY_LIFE_TIME}`,
   });
 
   return { token, refreshToken };
@@ -21,7 +16,7 @@ const createToken = async (username: string) => {
 const verifyToken = async (res: any, token: string) => {
   if (!token) res.status(404).json({ message: "No Token Found" });
 
-  jwt.verify(token, secret_key, (err, user) => {
+  jwt.verify(token, `${process.env.SECRET_KEY}`, (err, user) => {
     if (err) res.status(400).json({ message: "Invalid Token" });
   });
 };

4a74f22a565dd983fa7747f46355acf39856b9a6
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-10-13 17:23:02 +0330

    Login Code Moved to Component

--- login-web3-jwt/client/src/App.tsx
+++ login-web3-jwt/client/src/App.tsx
@@ -1,63 +1,13 @@
 import React from "react";
 
-import useUser from "./libs/use-user";
-import { Login, Logout } from "./libs/auth";
+import LoginBtn from "./components/login";
 import "./css/App.css";
 
 function App() {
-  const { user, loading, loggedOut, mutate } = useUser();
-  let interval: any;
-
-  const refreshToken = async () => {
-    if (localStorage.getItem("user") === null) return;
-
-    await fetch(`${import.meta.env.VITE_SERVER_PATH}user/refresh`, {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        username: localStorage.getItem("user"),
-        refresh: localStorage.getItem("refresh"),
-      }),
-    })
-      .then((res) => res.json())
-      .then((d) => {
-        if (d.connected === false) {
-          localStorage.removeItem("user");
-          localStorage.removeItem("token");
-          localStorage.removeItem("refresh");
-        } else {
-          localStorage.setItem("token", d.token);
-        }
-      });
-  };
-
-  React.useEffect(() => {
-    if (localStorage.getItem("user") === null) {
-      clearInterval(interval);
-    } else {
-      interval = setInterval(() => {
-        refreshToken();
-      }, 1000 * import.meta.env.VITE_SECRET_KEY_LIFE_TIME);
-    }
-  });
-
-  const walletClick = async () => {
-    if (!loggedOut === true) {
-      Logout();
-      mutate(() => null);
-    } else {
-      Login();
-      mutate();
-    }
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={walletClick}>
-          {!loggedOut === true ? "Logout" : "Login"}
-        </button>
-        <p>{loading ? "loading..." : !loggedOut === true ? user?.name : ""}</p>
+        <LoginBtn />
       </header>
     </div>
   );

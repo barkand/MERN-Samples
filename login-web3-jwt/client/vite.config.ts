27969ff7a2fddb7121809c6df1b908bd8180bbe3
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-11-10 01:21:09 +0330

    Add React to vite.config

--- login-web3-jwt/client/vite.config.ts
+++ login-web3-jwt/client/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   // ...other config settings
   optimizeDeps: {
     esbuildOptions: {
